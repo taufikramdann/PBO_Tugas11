7d4e1beea806d0f1f1a6f5c1e7da53ebb5777e13
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2023-01-30 19:43:55 +0700

    Create RandomAccessRevisi.java

---
+++ Praktikum_2/RandomAccessRevisi.java
@@ -0,0 +1,33 @@
+//Name : Taufik Ramdan
+//NIM : 20210040154
+//Class : TI21f
+
+package Nusput.Praktikum_2;
+
+import java.io.*;
+import java.lang.Exception;
+
+public class RandomAccessRevisi extends Exception{
+    public static void main(String[] args) {
+        String bookList[]={"Satu","Dua","Tiga"};
+        int yearList[]={1920,1230,1940};
+        try
+        {
+            RandomAccessFile books = new RandomAccessFile ("books.txt","rw");
+            for(int i=0;i<3;i++)
+            {
+                books.writeUTF(bookList[i]);
+                books.writeInt(yearList[i]);
+            }
+            books.seek(0);
+            System.out.println(books.readUTF()+" "+books.readInt());
+            System.out.println(books.readUTF()+ " "+books.readInt());
+            books.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Indeks melebihi batas");
+        }
+            System.out.println("test");
+        }
+}
